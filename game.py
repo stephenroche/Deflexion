50fd14e405ba74f7d69feb75a14cd7b1699dc028
Stephen Roche <stephen.roche00@gmail.com>
2020-03-02 11:48:56 +1100

    Board printing working

--- game.py
+++ game.py
@@ -16,21 +16,16 @@ class BoardState(dict):
 	def __getitem__(self, key):
 		return self.setdefault(key, None)
 
-	# def add_piece(self, piece, position):
-	# 	self[position] = piece
-
 	def set_start_state(self):
 		self.clear()
-		# dict.clear()
 
-		# Check these positions
 		for team, position in [(0, (4, 0)), (1, (5, 7))]:
 			self[position] = Pharaoh(team)
 
 		for team, position in [(0, (3, 0)), (0, (5, 0)), (1, (4, 7)), (1, (6, 7))]:
 			self[position] = Obelisk(team)
 
-		for team, position, aspect in [(0, (2, 0), 'NW'), (0, (2, 3), 'NW'), (0, (2, 4), 'SW'), (0, (3, 5), 'NW'), (0, (7, 1), 'NE'), (0, (9, 3), 'SW'), (0, (9, 4), 'NW'), (0, (7, 7), 'SE'), (0, (7, 4), 'SE'), (0, (7, 3), 'NE'), (0, (6, 2), 'SE'), (0, (2, 6), 'SW'), (0, (0, 4), 'NE'), (0, (0, 3), 'SE')]:
+		for team, position, aspect in [(0, (2, 0), 'NW'), (0, (2, 3), 'NW'), (0, (2, 4), 'SW'), (0, (3, 5), 'NW'), (0, (7, 1), 'NE'), (0, (9, 3), 'SW'), (0, (9, 4), 'NW'), (1, (7, 7), 'SE'), (1, (7, 4), 'SE'), (1, (7, 3), 'NE'), (1, (6, 2), 'SE'), (1, (2, 6), 'SW'), (1, (0, 4), 'NE'), (1, (0, 3), 'SE')]:
 			self[position] = Pyramid(team, aspect)
 
 		for team, position, aspect in [(0, (4, 3), 'NW'), (0, (5, 3), 'NE'), (1, (4, 4), 'NE'), (1, (5, 4), 'NW')]:
@@ -43,25 +38,54 @@ class BoardState(dict):
 			if isinstance(piece, Pharaoh):
 				pharaohs_found[piece.team] = True
 
-			if pharaohs_found[0] and pharaohs_found[1]:
-				return False
+		if pharaohs_found[0] and pharaohs_found[1]:
+			return False
 
-			elif pharaohs_found[0]:
-				return 'Win_0'
+		elif pharaohs_found[0]:
+			return 'Win_0'
 
-			elif pharaohs_found[1]:
-				return 'Win_1'
+		elif pharaohs_found[1]:
+			return 'Win_1'
 
-			else:
-				return 'Error: no pharaohs'
+		else:
+			return 'Error: no pharaohs'
 
 
 	def get_successor_states(self):
 		pass
 
 	def __str__(self):
-		pass
+		str_list = []
+		width = 10
+		height = 8
 
+		str_list.append('\u250C\u2500')
+		str_list.append(colored('\u2B07', 'red'))
+		for _ in range(width * 2 - 1):
+			str_list.append('\u2500')
+		str_list.append('\u2510\n')
+
+		for y in range(height - 1, -1, -1):
+			str_list.append('\u2502 ')
+			for x in range(width):
+				if (x, y) in self:
+					str_list.append(self[(x, y)].icon())
+				elif x == 0:
+					str_list.append(colored(' ', 'red', 'on_white'))
+				elif x == width - 1:
+					str_list.append(colored(' ', 'red', 'on_yellow'))
+				else:
+					str_list.append(' ')
+				str_list.append(' ')
+			str_list.append('\u2502\n')
+
+		str_list.append('\u2514')
+		for _ in range(width * 2 - 1):
+			str_list.append('\u2500')
+		str_list.append(colored('\u2B06', 'red'))
+		str_list.append('\u2500\u2518\n')
+
+		return ''.join(str_list)
 
 class Piece:
 	def __init__(self, team, aspect=None):
@@ -73,6 +97,9 @@ class Piece:
 
 	def icon(self):
 		return util.raiseNotDefined()
+
+	def __str__(self):
+		return self.icon()
 
 	# Maybe remove and put in BoardState
 	def move(self, action):
@@ -109,18 +136,31 @@ class Pharaoh(Piece):
 	def get_actions(self):
 		return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW']
 
+	def icon(self):
+		colour = 'yellow' if self.team == 0 else 'white'
+		return colored('\u265A', colour)
+
 
 class Obelisk(Piece):
 	"""docstring for Obelisk"""
 	def get_actions(self):
 		return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW']
 
+	def icon(self):
+		colour = 'yellow' if self.team == 0 else 'white'
+		return colored('\u25A3', colour)
+
 
 class Pyramid(Piece):
 	"""docstring for Pyramid"""
-
 	def get_actions(self):
 		return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW', 'tL', 'tR']
+
+	def icon(self):
+		colour = 'yellow' if self.team == 0 else 'white'
+		characters = {'NE': '\u2B15', 'SE': '\u25E9', 'SW': '\u2B14', 'NW': '\u25EA'}
+
+		return colored(characters[self.aspect], colour)
 
 
 class Djed(Piece):
@@ -132,14 +172,26 @@ class Djed(Piece):
 		elif self.aspect == 'NW':
 			return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW', 'tR']
 
+	def icon(self):
+		colour = 'yellow' if self.team == 0 else 'white'
+		characters = {'NE': '\u29C5', 'NW': '\u29C4'}
 
-class colours:
-	GOLD = '\033[33m'
-	SILVER = '\033[37m'
-	RESET = '\033[39m'
-
-
+		return colored(characters[self.aspect], colour)
 
 
 
-print(colored('hello', 'yellow'), colored('\u0420\u043e\u0441', 'white'))
+
+print(colored('\u25A3 \u25E9 \u25EA   \u25EA \u25EA \u25EA \u25EA\n\u2B14 \u2B15 \u29C4 \u29C5 \u265A \u26CB \U0001F796 \u25A9 \u2B1B', 'yellow'), colored('\u25E9', 'white'))
+
+board = BoardState()
+print(board)
+board.set_start_state()
+print(board)
+print(board.is_win_state())
+board[(1, 0)] = Pyramid(0, 'SW')
+print(board)
+board.set_start_state()
+print(board)
+del board[(4, 0)]
+print(board)
+print(board.is_win_state())
