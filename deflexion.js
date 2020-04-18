4361cf78f2fb44330549821f31341f9e644b2c53
Stephen Roche <stephen.roche00@gmail.com>
2020-04-18 20:01:11 +1000

    Showing board

--- deflexion.js
+++ deflexion.js
@@ -1,14 +1,34 @@
+"use strict";
+
+const width = 10;
+const height = 8;
+// const rect = gameArea.canvas.getBoundingClientRect();
+
 var gameArea = {
-    canvas : document.createElement("canvas"),
+    canvas : document.getElementById("gameArea"),
     start : function() {
-        this.width = 10;
-        this.height = 8;
-        this.canvas.width = 100 * (this.width + 1);
-        this.canvas.height = 100 * (this.height + 1);
+        this.canvas.width = 100 * (width + 1);
+        this.canvas.height = 100 * (height + 1);
         this.ctx = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         //this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+        this.buttons = [new Button(this.canvas.width - 100, this.canvas.height - 25), new Button(100, 25)];
+        this.boardState = new BoardState();
+        this.rect = this.canvas.getBoundingClientRect();
+        this.canvas.addEventListener('mousedown', (e) => {
+            this.x = e.x - this.rect.left;
+            this.y = e.y - this.rect.top;
+            this.clicked = true;
+        });
+        this.canvas.addEventListener('mousemove', (e) => {
+            this.x = e.x - this.rect.left;
+            this.y = e.y - this.rect.top;
+        });
+        this.canvas.addEventListener('mouseup', (e) => {
+            this.x = false;
+            this.y = false;
+            this.clicked = false;
+        });
     },
     stop : function() {
         clearInterval(this.interval);
@@ -21,31 +41,63 @@ var gameArea = {
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.fillStyle = "#909090";
         this.ctx.fillRect(50, 50, this.canvas.width - 100, this.canvas.height - 100);
-        this.ctx.fillStyle = "#101010";
-        this.ctx.arc(1, 0.25, 0.2, 0, 2*Math.PI);
-        this.ctx.fill();
-        this.ctx.fillStyle = "#F00000";
-        this.ctx.arc(1, 0.25, 0.01, 0, 2*Math.PI);
-        this.ctx.fill();
 
-        
-        for (var i = 0; i < this.width; i++) {
-            for (var j = 0; j < this.height; j++) {
-                this.ctx.translate(0.5 + i, 0.5 + j);
+        for (var i = 0; i < width; i++) {
+            for (var j = 0; j < height; j++) {
+                this.ctx.translate(50 + 100 * i, 50 + 100 * j);
                 this.ctx.fillStyle = "#A0A0A0";
-                this.ctx.fillRect(0.05, 0.05, 0.95, 0.95);
+                this.ctx.fillRect(5, 5, 95, 95);
                 this.ctx.fillStyle = "#D0D0D0";
-                this.ctx.fillRect(0.05, 0.05, 0.9, 0.9);
-                this.ctx.translate(-(0.5 + i), -(0.5 + j));
+                this.ctx.fillRect(5, 5, 90, 90);
+                this.ctx.translate(-(50 + 100 * i), -(50 + 100 * j));
             }
         }
         this.ctx.scale(1 / this.size, 1 / this.size)
+    },
+    draw : function() {
+        this.drawBackground();
+        this.buttons[0].draw();
+        this.buttons[1].draw();
+        if (this.clicked) {
+            this.ctx.fillStyle = "#00FF00";
+            this.ctx.fillRect(this.x, this. y, 20, 20);
+        }
+    }
+}
+
+function Button(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 20;
+    this.isPressed = false;
+    this.draw = function() {
+        var ctx = gameArea.ctx;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        if (this.isPressed) {
+            ctx.fillStyle = "#F06060";
+        } else {
+            ctx.fillStyle = "#F00000";
+        }
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        ctx.strokeStyle = "#707070";
+        ctx.lineWidth = 4;
+        ctx.stroke();
+    }
+}
+
+class BoardState {
+
+    constructor() {
+        this.pieces = new Map();
     }
 }
 
 function updateGameArea() {
     gameArea.clear();
-    gameArea.drawBackground();
+    gameArea.draw();
 }
 
 
