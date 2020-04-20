ac6c702cbfc7afe71f4e97eea807038d7d9749de
Stephen Roche <stephen.roche00@gmail.com>
2020-04-20 12:06:05 +1000

    Displaying pieces

--- deflexion.js
+++ deflexion.js
@@ -1,17 +1,28 @@
 "use strict";
 
-const width = 10;
-const height = 8;
-// const rect = gameArea.canvas.getBoundingClientRect();
+const WIDTH = 10;
+const HEIGHT = 8;
+const ASPECTS = ['NE', 'SE', 'SW', 'NW'];
+const OPPOSITE = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'};
+const ANGLES = {'NE': 0, 'SE': Math.PI/2, 'SW': Math.PI, 'NW': 3*Math.PI/2};
+const GOLD_PIECE = "#FFF000";
+const DARK_GOLD_PIECE = "#BBB000";
+const SILVER_PIECE = "#F0F0F0";
+const DARK_SILVER_PIECE = "#B0B0B0";
+// const BACKGROUND = "#A0A0A0";
+
+function startGame() {              
+    gameArea.start();
+}
 
 var gameArea = {
     canvas : document.getElementById("gameArea"),
     start : function() {
-        this.canvas.width = 100 * (width + 1);
-        this.canvas.height = 100 * (height + 1);
+        this.canvas.width = 100 * (WIDTH + 1);
+        this.canvas.height = 100 * (HEIGHT + 1);
         this.ctx = this.canvas.getContext("2d");
         //this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(this.update.bind(this), 20);
         this.buttons = [new Button(this.canvas.width - 100, this.canvas.height - 25), new Button(100, 25)];
         this.boardState = new BoardState();
         this.rect = this.canvas.getBoundingClientRect();
@@ -19,6 +30,11 @@ var gameArea = {
             this.x = e.x - this.rect.left;
             this.y = e.y - this.rect.top;
             this.clicked = true;
+            for (var button of this.buttons) {
+                if (button.isClicked(this.x, this.y)) {
+                    button.isPressed = true;
+                }
+            }
         });
         this.canvas.addEventListener('mousemove', (e) => {
             this.x = e.x - this.rect.left;
@@ -28,7 +44,16 @@ var gameArea = {
             this.x = false;
             this.y = false;
             this.clicked = false;
+
+            for (var button of this.buttons) {
+                button.isPressed = false;
+            }
         });
+        this.boardState.pieces.set([1, 2], new Pyramid(0, 'SE'));
+        this.boardState.pieces.set([5, 3], new Pyramid(1, 'SW'));
+        this.boardState.pieces.set([3, 3], new Djed(0, 'NE'));
+        this.boardState.pieces.set([0, 0], new Obelisk(1));
+        this.boardState.pieces.set([4, 7], new Pharaoh(0));
     },
     stop : function() {
         clearInterval(this.interval);
@@ -39,28 +64,39 @@ var gameArea = {
     drawBackground : function() {
         this.ctx.fillStyle = "#303030";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.fillStyle = "#909090";
+        this.ctx.fillStyle = "#606060";
         this.ctx.fillRect(50, 50, this.canvas.width - 100, this.canvas.height - 100);
 
-        for (var i = 0; i < width; i++) {
-            for (var j = 0; j < height; j++) {
+        for (var i = 0; i < WIDTH; i++) {
+            for (var j = 0; j < HEIGHT; j++) {
                 this.ctx.translate(50 + 100 * i, 50 + 100 * j);
-                this.ctx.fillStyle = "#A0A0A0";
+                this.ctx.fillStyle = "#808080";
                 this.ctx.fillRect(5, 5, 95, 95);
-                this.ctx.fillStyle = "#D0D0D0";
+                if (i == 0) {
+                    this.ctx.fillStyle = "#F0F0F0";
+                } else if (i == WIDTH - 1) {
+                    this.ctx.fillStyle = "#F0D070";
+                } else{
+                    this.ctx.fillStyle = "#A0A0A0";
+                }
                 this.ctx.fillRect(5, 5, 90, 90);
                 this.ctx.translate(-(50 + 100 * i), -(50 + 100 * j));
             }
         }
         this.ctx.scale(1 / this.size, 1 / this.size)
     },
-    draw : function() {
+    update : function() {
+        this.clear();
         this.drawBackground();
         this.buttons[0].draw();
         this.buttons[1].draw();
-        if (this.clicked) {
-            this.ctx.fillStyle = "#00FF00";
-            this.ctx.fillRect(this.x, this. y, 20, 20);
+        this.drawPieces();
+    },
+    drawPieces : function() {
+        for (var [pos, piece] of this.boardState.pieces) {
+            var [x, y] = pos;
+            var rotation = 0;
+            piece.draw(this.ctx, 100 * (x + 1), 100 * (y + 1), rotation);
         }
     }
 }
@@ -86,21 +122,182 @@ function Button(x, y) {
         ctx.lineWidth = 4;
         ctx.stroke();
     }
-}
-
-class BoardState {
-
-    constructor() {
-        this.pieces = new Map();
+    this.isClicked = function(x, y) {
+        var distance = Math.sqrt((x - this.x)**2 + (y - this.y)**2);
+        return (distance <= this.radius);
     }
 }
 
-function updateGameArea() {
-    gameArea.clear();
-    gameArea.draw();
+class BoardState {
+    constructor(turn=0) {
+        this.pieces = new Map();
+        this.turn = turn;
+        this.num_turns = 0;
+    }
 }
 
+class Piece {
+    constructor(team, aspect=null) {
+        this.team = team;
+        this.aspect = aspect;
+    }
+    turn(direction) {
+        if (direction == 'L') {
+            this.aspect = ASPECTS[(ASPECTS.indexOf(this.aspect) + 3) % 4];
+        } else if (direction == 'R') {
+            this.aspect = ASPECTS[(ASPECTS.indexOf(this.aspect) + 1) % 4];
+        }
+    }
+    copy() {
+        return { ...this };
+    }
+    draw(ctx, x, y, rotation) {
+        if (this.aspect) {
+            rotation += ANGLES[this.aspect];
+        }
+        ctx.translate(x, y);
+        ctx.rotate(rotation);
+        this.drawIcon(ctx);
+        ctx.rotate(-rotation);
+        ctx.translate(-x, -y);
+    }
+    drawIcon(ctx) {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(-50, -50, 100, 100);
+    }
+}
 
-function startGame() {              
-    gameArea.start();
+class Pharaoh extends Piece {
+    constructor(team, aspect=null) {
+        super(team, aspect);
+        this.type = 'Pharaoh';
+    }
+    get_actions() {
+        return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW'];
+    }
+    drawIcon(ctx) {
+        if (this.team == 0) {
+            var light = GOLD_PIECE;
+            var dark = DARK_GOLD_PIECE;
+        } else {
+            var light = SILVER_PIECE;
+            var dark = DARK_SILVER_PIECE;
+        }
+
+        ctx.fillStyle = dark;
+        ctx.fillRect(-50, -50, 100, 100);
+
+        ctx.beginPath();
+        ctx.strokeStyle = light;
+        ctx.lineWidth = 10;
+        ctx.strokeRect(-45, -45, 90, 90);
+
+        ctx.beginPath();
+        ctx.moveTo(0, -20);
+        ctx.lineTo(20, 15);
+        ctx.lineTo(-20, 15);
+        ctx.closePath()
+        ctx.fillStyle = light;
+        ctx.fill();
+    }
+}
+
+class Obelisk extends Piece {
+    constructor(team, aspect=null) {
+        super(team, aspect);
+        this.type = 'Obelisk';
+    }
+    get_actions() {
+        return ['mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW'];
+    }
+    drawIcon(ctx) {
+        if (this.team == 0) {
+            var light = GOLD_PIECE;
+            var dark = DARK_GOLD_PIECE;
+        } else {
+            var light = SILVER_PIECE;
+            var dark = DARK_SILVER_PIECE;
+        }
+
+        ctx.fillStyle = dark;
+        ctx.fillRect(-50, -50, 100, 100);
+
+        ctx.beginPath();
+        ctx.strokeStyle = light;
+        ctx.lineWidth = 10;
+        ctx.strokeRect(-45, -45, 90, 90);
+
+        ctx.fillStyle = light;
+        ctx.fillRect(-30, -30, 60, 60);
+    }
+}
+
+class Pyramid extends Piece {
+    constructor(team, aspect=null) {
+        super(team, aspect);
+        this.type = 'Pyramid';
+    }
+    get_actions() {
+        return ['tL', 'tR', 'mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW'];
+    }
+    drawIcon(ctx) {
+        if (this.team == 0) {
+            var light = GOLD_PIECE;
+            var dark = DARK_GOLD_PIECE;
+        } else {
+            var light = SILVER_PIECE;
+            var dark = DARK_SILVER_PIECE;
+        }
+
+        ctx.fillStyle = dark;
+        ctx.fillRect(-50, -50, 100, 100);
+
+        ctx.beginPath();
+        ctx.moveTo(-50, -50);
+        ctx.lineTo(50, 50);
+        ctx.lineTo(-50, 50);
+        ctx.closePath();
+        ctx.fillStyle = light;
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.rect(-45, -45, 90, 90);
+        ctx.lineTo(45, 45);
+        ctx.strokeStyle = light;
+        ctx.lineWidth = 10;
+        ctx.stroke();
+    }
+}
+
+class Djed extends Piece {
+    constructor(team, aspect=null) {
+        super(team, aspect);
+        this.type = 'Djed';
+    }
+    get_actions() {
+        if (this.aspect == 'NE') {
+            return ['tL', 'mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW'];
+        } else if (this.aspect == 'NW') {
+            return ['tR', 'mN', 'mNE', 'mE', 'mSE', 'mS', 'mSW', 'mW', 'mNW'];
+        }
+    }
+    drawIcon(ctx) {
+        if (this.team == 0) {
+            var light = GOLD_PIECE;
+            var dark = DARK_GOLD_PIECE;
+        } else {
+            var light = SILVER_PIECE;
+            var dark = DARK_SILVER_PIECE;
+        }
+
+        ctx.fillStyle = dark;
+        ctx.fillRect(-50, -50, 100, 100);
+
+        ctx.beginPath();
+        ctx.rect(-45, -45, 90, 90);
+        ctx.lineTo(45, 45);
+        ctx.strokeStyle = light;
+        ctx.lineWidth = 10;
+        ctx.stroke();
+    }
 }
